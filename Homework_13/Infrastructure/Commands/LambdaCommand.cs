f73729a804b011ad70652f529fc108cdec91b6a5
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-09-15 09:26:26 +0300

    Заготовка MVVM

---
+++ Homework_13/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Homework_13.Infrastructure.Commands.Base;
+
+namespace Homework_13.Infrastructure.Commands;
+
+internal class LambdaCommand : Command
+{
+    private readonly Action<object> _Execute;
+    private readonly Func<object, bool> _CanExecute;
+
+
+    public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+    {
+        _Execute = Execute ?? throw new ArgumentException(nameof(Execute));
+        _CanExecute = CanExecute;
+    }
+
+    public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true; 
+    
+    public override void Execute(object parameter) => _Execute(parameter);
+    
+}
