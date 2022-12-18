7083d340f4f1fbf551ce0d0aceb4602ab672673b
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-12-18 12:45:27 +0300

    Скорректировал список клиентов

---
+++ Homework_13/Infrastructure/Convertors/ClientToFioConvertor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Homework_13.Models.Clients;
+
+namespace Homework_13.Infrastructure.Convertors;
+
+public class ClientToFioConvertor : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (!(value is Client client)) return null;
+        return $"{client.LastName} {client.FirstName} {client.MiddleName}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Homework_13/Infrastructure/Convertors/PassportToStringConvertor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Homework_13.Infrastructure.Convertors;
+
+public class PassportToStringConvertor : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
